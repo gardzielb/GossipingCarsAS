b123c8d679cc76fc014299b2d64bcf34b683ac39
Paweł Dębski <52053209+debskipawel@users.noreply.github.com>
2021-05-23 19:37:58 +0200

    Added decoded route models

---
+++ AgentSystem/src/main/java/com/kgd/agents/models/DecodedRoute.java
@@ -0,0 +1,18 @@
+package com.kgd.agents.models;
+
+import java.util.ArrayList;
+
+public class DecodedRoute {
+    public GeoPoint origin;
+    public String destinationId;
+    public ArrayList<DecodedRouteSegment> segments = new ArrayList<>();
+
+    public DecodedRoute(Route route) {
+        origin = route.origin();
+        destinationId = route.destinationId();
+
+        for (var segment: route.segments()) {
+            segments.add(new DecodedRouteSegment(segment));
+        }
+    }
+}
