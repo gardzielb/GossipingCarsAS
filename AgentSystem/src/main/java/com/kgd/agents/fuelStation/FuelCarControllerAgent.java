f4e35983949e8e7ba7fc56b88266c3523ad8de1f
Bartosz Gardziel <01142169@pw.edu.pl>
2021-05-26 22:46:03 +0200

    Created initial implementation of PriceNegotiationInitiatorBehavior

---
+++ AgentSystem/src/main/java/com/kgd/agents/fuelStation/FuelCarControllerAgent.java
@@ -0,0 +1,10 @@
+package com.kgd.agents.fuelStation;
+
+import jade.core.Agent;
+
+public class FuelCarControllerAgent extends Agent {
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+}
---
+++ AgentSystem/src/main/java/com/kgd/agents/fuelStation/PriceNegotiationInitiatorBehavior.java
@@ -0,0 +1,82 @@
+package com.kgd.agents.fuelStation;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.proto.ContractNetInitiator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+public class PriceNegotiationInitiatorBehavior extends ContractNetInitiator {
+
+    private final List<PriceSuggestion> priceSuggestions = new ArrayList<>();
+    private PriceSuggestion bestPrice;
+
+    public PriceNegotiationInitiatorBehavior(Agent agent, ACLMessage cfp, List<PriceSuggestion> priceSuggestions) {
+        super(agent, cfp);
+        this.priceSuggestions.addAll(priceSuggestions);
+        this.bestPrice = priceSuggestions.get(0);
+    }
+
+    @Override
+    protected Vector prepareCfps(ACLMessage cfp) {
+        var cfpVector = new Vector<ACLMessage>(priceSuggestions.size());
+        priceSuggestions.forEach(suggestion -> cfpVector.add(
+                createCfp(suggestion.stationAid(), String.format(Locale.ROOT, "%.2f", suggestion.price())))
+        );
+        return cfpVector;
+    }
+
+    @Override
+    protected void handleAllResponses(Vector responses, Vector acceptances) {
+        super.handleAllResponses(responses, acceptances);
+
+        List<ACLMessage> proposals = findPriceProposals(responses);
+
+        if (proposals.isEmpty()) {
+            System.out.println("No proposals");
+        }
+        else if (proposals.size() == 1) {
+            acceptances.add(createAcceptance(proposals.get(0)));
+        }
+        else {
+            var nextRoundCfp = new Vector<ACLMessage>();
+            proposals.forEach(
+                    proposal -> nextRoundCfp.add(createCfp(proposal.getSender(), proposal.getContent()))
+            );
+            newIteration(nextRoundCfp);
+        }
+    }
+
+    @Override
+    public int onEnd() {
+        System.out.println("Ending contract net");
+        return super.onEnd();
+    }
+
+    private List<ACLMessage> findPriceProposals(Vector<ACLMessage> responses) {
+        return responses.stream()
+                        .filter(r -> r.getPerformative() == ACLMessage.PROPOSE)
+                        .collect(Collectors.toList());
+    }
+
+    private ACLMessage createAcceptance(ACLMessage proposal) {
+        var acceptMsg = proposal.createReply();
+        acceptMsg.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+        acceptMsg.setProtocol(FIPANames.InteractionProtocol.FIPA_ITERATED_CONTRACT_NET);
+        return acceptMsg;
+    }
+
+    private ACLMessage createCfp(AID receiver, String content) {
+        var cfp = new ACLMessage(ACLMessage.CFP);
+        cfp.setProtocol(FIPANames.InteractionProtocol.FIPA_ITERATED_CONTRACT_NET);
+        cfp.addReceiver(receiver);
+        cfp.setContent(content);
+        return cfp;
+    }
+}
