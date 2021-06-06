61d39afc1978e2c5b8ee6a2d38d591902c125079
gardzielb <bartosz.g.1999@gmail.com>
2021-06-06 14:26:14 +0200

    Implemented TrafficLightsCarControllerAgent creation and new route notifications

--- AgentSystem/src/main/java/com/kgd/agents/Main.java
+++ AgentSystem/src/main/java/com/kgd/agents/Main.java
@@ -18,25 +18,25 @@ public class Main {
     public static void main(String[] args)
             throws StaleProxyException, URISyntaxException, IOException, InterruptedException {
         jade.Boot.main(new String[]{"-gui", "factory:com.kgd.agents.carManufacturer.CarManufacturerAgent"});
-//        Runtime runtime = Runtime.instance();
-//
-//        PlaceService placeService = new HttpPlaceService();
-//        var stations = placeService.findAllByType(PlaceType.GAS_STATION);
-//
-//        Profile p = new ProfileImpl();
-//        p.setParameter(Profile.CONTAINER_NAME, "FuelStations");
-//        AgentContainer container = runtime.createAgentContainer(p);
-//
-//        for (var station : stations) {
-//            AgentController agent = container.createNewAgent(
-//                    station.id(),
-//                    "com.kgd.agents.fuelStation.FuelStationManagerAgent",
-//                    new Object[]{
-//                            station.location(),
-//                            station.id()
-//                    }
-//            );
-//            agent.start();
-//        }
+        Runtime runtime = Runtime.instance();
+
+        PlaceService placeService = new HttpPlaceService();
+        var stations = placeService.findAllByType(PlaceType.GAS_STATION);
+
+        Profile p = new ProfileImpl();
+        p.setParameter(Profile.CONTAINER_NAME, "FuelStations");
+        AgentContainer container = runtime.createAgentContainer(p);
+
+        for (var station : stations) {
+            AgentController agent = container.createNewAgent(
+                    station.id(),
+                    "com.kgd.agents.fuelStation.FuelStationManagerAgent",
+                    new Object[]{
+                            station.location(),
+                            station.id()
+                    }
+            );
+            agent.start();
+        }
     }
 }
